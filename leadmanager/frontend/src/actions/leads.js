9d4a8b8e08bddf62bcf99efa3082d270bfd646f5
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-02-01 06:51:12 +1300

    add lead
    add lead

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_LEADS, DELETE_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 export const getLeads = () => dispatch => {
   axios
@@ -20,6 +20,18 @@ export const deleteLead = id => dispatch => {
       dispatch({
         type: DELETE_LEAD,
         payload: id
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+export const addLead = lead => dispatch => {
+  axios
+    .post(`/api/leads/`, lead)
+    .then(response => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: response.data
       });
     })
     .catch(err => console.log(err));

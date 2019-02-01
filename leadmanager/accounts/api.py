ab4371b5fe87360db2b563dd9cd44454decd23d0
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-02 12:53:58 +1300

    users saving to db

--- leadmanager/accounts/api.py
+++ leadmanager/accounts/api.py
@@ -14,9 +14,21 @@ class RegisterAPI(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({
-            "user": UserSerializer(user, context=self.get_serializer_context().data),
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)
         })
 # Login API
 
+
+# class RegisterAPI(generics.GenericAPIView):
+#     serializer_class = RegisterSerializer
+
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.save()
+#         return Response({
+#             "user": UserSerializer(user, context=self.get_serializer_context()).data,
+#             "token": AuthToken.objects.create(user)
+#         })
 # Get User API

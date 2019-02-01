7c65b27bae5f062f0a74bb94356db0b265eeebd8
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-02 12:42:03 +1300

    accounts added

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { createMessage } from "./messages";
+import { createMessage, returnErrors } from "./messages";
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 export const getLeads = () => dispatch => {
   axios
@@ -12,7 +12,9 @@ export const getLeads = () => dispatch => {
         payload: response.data
       });
     })
-    .catch(err => console.log(err));
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 export const deleteLead = id => dispatch => {
@@ -38,14 +40,7 @@ export const addLead = lead => dispatch => {
         payload: response.data
       });
     })
-    .catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      };
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };

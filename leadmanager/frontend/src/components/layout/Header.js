f653f143162cd872e89ac277930526afd989fded
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-02 17:12:17 +1300

    private routes

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -19,7 +19,7 @@ export class Header extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <a className="navbar-brand" href="#">
-              Lead Manager Header
+              Lead Manager
             </a>
             <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
               <li className="nav-item">
